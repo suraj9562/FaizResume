d238610e419d9b132a72de55e6175616a387b6c6
@suraj05 <wadikarsuraj02@gmail.com>
2022-11-24 23:15:01 +0530

    done with intro section

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -6,6 +6,16 @@ function Intro() {
   return (
     <div className={cssClasses.container}>
       <Header />
+      <div className={cssClasses.content}>
+        <div className={cssClasses.hi}>HEY THERE!</div>
+        <div className={cssClasses.name}>I’M FAIZ SAIYAD</div>
+        <div className={cssClasses.profession}>DATA SCIENTIST</div>
+        <div className={cssClasses.subtitle}>
+          A Data Scientist with experience in Full Stack development using
+          Python. Aiming to improve in terms of knowledge in the field of AI
+        </div>
+        <div className={cssClasses.button}>DOWNLOAD CV</div>
+      </div>
     </div>
   );
 }
